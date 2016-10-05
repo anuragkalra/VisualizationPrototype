01bc685c7ffc6fb96325d32e347372356c768041
anuragkalra <anuragkalra@outlook.com>
2016-10-05 17:05:18 -0400

    created constructor for Pixel class

--- VisualizationPrototype/src/Pixel.java
+++ VisualizationPrototype/src/Pixel.java
@@ -3,4 +3,10 @@ public class Pixel {
 	private int RED_VAL;
 	private int GREEN_VAL;
 	private int BLUE_VAL;
+	
+	public Pixel(int aRED_VAL, int aGREEN_VAL, int aBLUE_VAL){
+		this.RED_VAL = aRED_VAL;
+		this.GREEN_VAL = aGREEN_VAL;
+		this.BLUE_VAL = aBLUE_VAL;
+	}
 }
