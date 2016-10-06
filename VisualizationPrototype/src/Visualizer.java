f639f5e8a147c5f70175d557c8547e22fe3dc267
anuragkalra <anuragkalra@outlook.com>
2016-10-06 14:16:32 -0400

    Added global variables DIMENSION and SPEED to Visualizer class.

--- VisualizationPrototype/src/Visualizer.java
+++ VisualizationPrototype/src/Visualizer.java
@@ -1,6 +1,8 @@
 
 public class Visualizer {
-
+	public static int DIMENSION = 5;	//THE LENGTH AND THE WIDTH OF THE SQUARE CANVAS
+	public static int SPEED = 10;	//AFFECTS THE WAIT TIME BETWEEN EACH STEP OF THE FILTER OPERATIONS
+	
 	public static void main(String[] args){
 		
 	}
