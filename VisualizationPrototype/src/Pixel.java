941ba471616719e96bf4368befcf0fe10cf3d2b8
anuragkalra <anuragkalra@outlook.com>
2016-10-06 13:52:22 -0400

    Added Getters + Setters for Pixel class. Added Board + Filter class.

--- VisualizationPrototype/src/Pixel.java
+++ VisualizationPrototype/src/Pixel.java
@@ -9,8 +9,47 @@ public class Pixel {
 		this.GREEN_VAL = aGREEN_VAL;
 		this.BLUE_VAL = aBLUE_VAL;
 	}
-	
-	public void setRED_VAL(int aRED_VAL){
-		this.RED_VAL = aRED_VAL;
+
+	/**
+	 * @return the RED_VAL
+	 */
+	public int getRED_VAL() {
+		return RED_VAL;
 	}
+
+	/**
+	 * @return the GREEN_VAL
+	 */
+	public int getGREEN_VAL() {
+		return GREEN_VAL;
+	}
+
+	/**
+	 * @return the BLUE_VAL
+	 */
+	public int getBLUE_VAL() {
+		return BLUE_VAL;
+	}
+
+	/**
+	 * @param RED_VAL the RED_VAL to set
+	 */
+	public void setRED_VAL(int aRED_VAL) {
+		RED_VAL = aRED_VAL;
+	}
+
+	/**
+	 * @param GREEN_VAL the GREEN_VAL to set
+	 */
+	public void setGREEN_VAL(int aGREEN_VAL) {
+		GREEN_VAL = aGREEN_VAL;
+	}
+
+	/**
+	 * @param BLUE_VAL the BLUE_VAL to set
+	 */
+	public void setBLUE_VAL(int aBLUE_VAL) {
+		BLUE_VAL = aBLUE_VAL;
+	}
+	
 }
