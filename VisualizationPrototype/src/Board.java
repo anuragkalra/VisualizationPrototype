10beab9ffec6ee6b53f58a5d317e4e06fd0dd1c4
anuragkalra <anuragkalra@outlook.com>
2016-10-06 14:20:44 -0400

    Added fillBoardLight() and fillBoardDark() methods to Board class.

--- VisualizationPrototype/src/Board.java
+++ VisualizationPrototype/src/Board.java
@@ -1,4 +1,20 @@
 
 public class Board {
-
+	Pixel[][] Grid = new Pixel[Visualizer.DIMENSION][Visualizer.DIMENSION];
+	
+	public void fillBoardLight(){
+		for(Pixel[] line : Grid){
+			for(Pixel p: line){
+				p.setALL(Pixel.MAX_LIGHT, Pixel.MAX_LIGHT, Pixel.MAX_LIGHT);
+			}
+		}
+	}
+	
+	public void fillBoardDark(){
+		for(Pixel[] line : Grid){
+			for(Pixel p: line){
+				p.setALL(Pixel.MIN_LIGHT, Pixel.MIN_LIGHT, Pixel.MIN_LIGHT);
+			}
+		}
+	}
 }
